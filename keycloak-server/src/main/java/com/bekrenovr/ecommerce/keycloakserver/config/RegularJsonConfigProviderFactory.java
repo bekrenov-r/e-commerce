6b2eb010f2e2d6b881c7c35c5df82f055aa2eda4
bekrenov-r <bekrenov.roman@gmail.com>
2024-07-05 12:23:03 +0200

    Setup keycloak authorization server

---
+++ keycloak-server/src/main/java/com/bekrenovr/ecommerce/keycloakserver/config/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,6 @@
+package com.bekrenovr.ecommerce.keycloakserver.config;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+}
