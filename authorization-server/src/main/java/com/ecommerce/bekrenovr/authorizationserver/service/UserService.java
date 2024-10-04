61d4019085769d967c8a65d7209d97891f0dcbee
bekrenov-r <bekrenov.roman@gmail.com>
2024-10-04 17:30:45 +0200

    Implement password recovery flow for customer who was earlier registered via Google

--- authorization-server/src/main/java/com/ecommerce/bekrenovr/authorizationserver/service/UserService.java
+++ authorization-server/src/main/java/com/ecommerce/bekrenovr/authorizationserver/service/UserService.java
@@ -1,8 +1,10 @@
 package com.ecommerce.bekrenovr.authorizationserver.service;
 
 import com.bekrenovr.ecommerce.common.model.Person;
+import com.bekrenovr.ecommerce.common.security.Role;
 import com.ecommerce.bekrenovr.authorizationserver.proxy.CustomerServiceProxy;
 import com.ecommerce.bekrenovr.authorizationserver.proxy.KeycloakProxy;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
@@ -17,7 +19,16 @@ public class UserService {
     public void sendEmailForPasswordRecovery(String email) {
         Person person = customerServiceProxy.getCustomerByEmail(email).getBody();
         String recoveryToken = RandomStringUtils.random(20, true, true);
-        keycloakProxy.createPasswordRecoveryToken(person.getEmail(), recoveryToken);
+        try {
+            keycloakProxy.createPasswordRecoveryToken(person.getEmail(), recoveryToken);
+        } catch(FeignException.NotFound ex) {
+            String tempPassword = RandomStringUtils.random(20, true, true);
+            String activationToken = keycloakProxy
+                    .createKeycloakUser(person.getEmail(), tempPassword, Role.CUSTOMER.name(), person.getFirstName())
+                    .getBody();
+            keycloakProxy.enableUser(activationToken);
+            keycloakProxy.createPasswordRecoveryToken(person.getEmail(), recoveryToken);
+        }
         mailService.sendPasswordRecoveryEmail(person, recoveryToken);
     }
 
