5debd6b93eb5f0f3302f07e22013910666039d6c
bekrenov-r <bekrenov.roman@gmail.com>
2024-11-13 16:02:29 +0100

    Setup Spring Cloud Config Server with profiles 'git' (fetch configuration from remote git repository) and 'native' (store configuration in file system); Connect config server to eureka; Update catalog service to use configuration from config server (except for test profile, for which application-test.yml was created)

--- config-server/src/main/java/com/bekrenovr/ecommerce/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/bekrenovr/ecommerce/configserver/ConfigServerApplication.java
@@ -4,12 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.io.IOException;
+import java.util.Properties;
+
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication app = new SpringApplication(ConfigServerApplication.class);
+		app.setDefaultProperties(getDefaultProperties());
+		app.run(args);
 	}
 
+	private static Properties getDefaultProperties() {
+		try {
+			String key = new String(ConfigServerApplication.class.getResourceAsStream("/key.txt").readAllBytes());
+			Properties p = new Properties();
+			p.put("encrypt.key", key);
+			return p;
+		} catch(IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 }
